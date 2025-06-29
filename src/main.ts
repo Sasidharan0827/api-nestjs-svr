cd5a96cbf3b78814c2af4b7abfe25a0cad570f22
sasidharan0827 <sasidharan0827@gmail.com>
2025-06-29 21:54:22 +0530

    server port

--- src/main.ts
+++ src/main.ts
@@ -59,6 +59,7 @@ async function bootstrap() {
 
   // Start the application
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
