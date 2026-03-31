3bc4b18812be47a41e829e8e7d511a8683087f32
sasidharan0827 <sasidharan0827@gmail.com>
2026-03-31 16:40:07 +0530

    prettier-off

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,42 +1,41 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from './users/users.module';
-import { User } from './users/entities/user.entity';
-import { AuthModule } from './auth/auth.module';
-
-import { JwtModule } from '@nestjs/jwt';
-import { register } from 'module';
-import { SignUpDto } from './auth/dto/auth.dto';
-import { APP_GUARD } from '@nestjs/core';
-import { AuthGuard } from './auth/auth.gaurd.';
-import { DoctorModule } from './doctor/doctor.module';
-import { Doctor } from './doctor/entities/doctor.entity';
-import { AppoinmentModule } from './appoinment/appoinment.module';
-import { Appoinment } from './appoinment/entities/appoinment.entity';
-import { ConsultationModule } from './consultation/consultation.module';
-import { Consultation } from './consultation/entities/consultation.entity';
-import { AdminModule } from './admin/admin.module';
-import { Admin } from './admin/entities/admin.entity';
-import { UploadImagesModule } from './upload-images/upload-images.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UsersModule } from "./users/users.module";
+import { User } from "./users/entities/user.entity";
+import { AuthModule } from "./auth/auth.module";
+import { JwtModule } from "@nestjs/jwt";
+// import { register } from "module";
+// import { SignUpDto } from "./auth/dto/auth.dto";
+import { APP_GUARD } from "@nestjs/core";
+import { AuthGuard } from "./auth/auth.gaurd.";
+import { DoctorModule } from "./doctor/doctor.module";
+import { Doctor } from "./doctor/entities/doctor.entity";
+import { AppoinmentModule } from "./appoinment/appoinment.module";
+import { Appoinment } from "./appoinment/entities/appoinment.entity";
+import { ConsultationModule } from "./consultation/consultation.module";
+import { Consultation } from "./consultation/entities/consultation.entity";
+import { AdminModule } from "./admin/admin.module";
+import { Admin } from "./admin/entities/admin.entity";
+import { UploadImagesModule } from "./upload-images/upload-images.module";
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
+      type: "mysql",
+      host: "localhost",
       port: 3005,
-      username: 'admin',
-      password: 'admin123',
-      database: 'blog',
+      username: "admin",
+      password: "admin123",
+      database: "blog",
       entities: [User, Doctor, Appoinment, Consultation, Admin],
       synchronize: true,
       logging: true,
     }),
     UsersModule,
     AuthModule,
-    JwtModule.register({ secret: 'secret' }),
+    JwtModule.register({ secret: "secret" }),
     DoctorModule,
     AppoinmentModule,
     ConsultationModule,
