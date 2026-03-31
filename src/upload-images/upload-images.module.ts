d1ffd4214ee740699f8ccdf778ff516136a6b0ce
sasidharan0827 <sasidharan0827@gmail.com>
2026-03-31 18:50:33 +0530

    render-fix

--- src/upload-images/upload-images.module.ts
+++ src/upload-images/upload-images.module.ts
@@ -1,16 +1,8 @@
-import { Module } from '@nestjs/common';
-import { UploadImagesService } from './upload-images.service';
-import { UploadImagesController } from './upload-images.controller';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
+import { Module } from "@nestjs/common";
+import { UploadImagesService } from "./upload-images.service";
+import { UploadImagesController } from "./upload-images.controller";
 
 @Module({
-  imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'uploads'), 
-      serveRoot: '/uploads/', 
-    }),
-  ],
   controllers: [UploadImagesController],
   providers: [UploadImagesService],
 })
